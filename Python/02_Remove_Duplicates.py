cacc5ec138f348262399a995d66f37234921081c
Bhoomija-AI <trbhoomija2004@gmail.com>
2026-04-09 19:03:30 +0530

    Update 02_Remove_Duplicates.py

--- Python/02_Remove_Duplicates.py
+++ Python/02_Remove_Duplicates.py
@@ -1,1 +1,12 @@
+# Remove duplicate characters from a string using loop
+
+text = input("Enter string: ")
+
+result = ""
+
+for char in text:
+    if char not in result:
+        result += char
+
+print("Unique string:", result)
 
